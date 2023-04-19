8c350b07bfb567affc988f14a650c9e5de3674a1
redth <jondick@gmail.com>
2023-04-19 13:23:03 -0400

    Added Issue 14624

---
+++ MauiReproBrowser/Issues/14624/Issue14624.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace MauiReproBrowser.Issues._14624;
+
+[Issue(14624, "[regression/7.0.81] ScrollView content with Buttons rendered offscreen do not respond on iOS")]
+public partial class Issue14624 : ContentPage
+{
+	public Issue14624()
+	{
+		InitializeComponent();
+
+		for (int i = 0; i < 30; i++)
+		{
+			var btn = new Button { Text = $"Button {i}" };
+            btn.Clicked += Btn_Clicked;
+			stack.Add(btn);
+		}
+	}
+
+    async void Btn_Clicked(object sender, EventArgs e)
+    {
+		var btn = sender as Button;
+
+		if (btn is null)
+			return;
+
+		await DisplayAlert("Clicked", $"{btn.Text} clicked", "OK");
+    }
+}
