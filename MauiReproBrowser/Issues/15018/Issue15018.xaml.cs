a63d5e7f5f9f86f55dff314f4d456e31854ad397
redth <jondick@gmail.com>
2023-05-10 14:22:07 -0400

    Add Issue #15018

---
+++ MauiReproBrowser/Issues/15018/Issue15018.xaml.cs
@@ -0,0 +1,10 @@
+namespace MauiReproBrowser.Issues._15018;
+
+[Issue(15018, "[iOS/Catalyst] Grid issue with calculating * values on iOS/MacCatalyst")]
+public partial class Issue15018 : ContentPage
+{
+	public Issue15018()
+	{
+		InitializeComponent();
+	}
+}
